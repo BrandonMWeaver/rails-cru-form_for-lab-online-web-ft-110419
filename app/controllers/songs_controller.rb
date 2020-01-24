52c528dfb1088814c84cf779555de1dea142ece5
Brandon Weaver <a7xncob@gmail.com>
2020-01-24 01:28:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,9 @@ class SongsController < ApplicationController
   end
   
   def update
+    song = Song.find(params[:id])
+    song.update(song_params(:name))
+    redirect_to song_path(song)
   end
   
   private
