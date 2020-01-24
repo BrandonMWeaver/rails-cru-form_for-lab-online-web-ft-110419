20e38a196dad4e1487e2efc1916e48aef2229b11
Brandon Weaver <a7xncob@gmail.com>
2020-01-24 01:09:12 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,6 +5,8 @@ class ArtistsController < ApplicationController
   end
   
   def create
+    artist = Artist.create(artist_params(:name, :bio))
+    redirect_to artist_path(artist)
   end
   
   def show
